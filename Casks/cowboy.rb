7625e5c842fa0bc9aea00005997cf9fe21086b17
Sachin Rajput <sachin.rajput.k@gmail.com>
2026-04-18 01:02:07 +0530

    Update Cowboy to v3.5.2

--- Casks/cowboy.rb
+++ Casks/cowboy.rb
@@ -1,8 +1,8 @@
 cask "cowboy" do
-  version "3.5.1"
-  sha256 "f9eafdd555f7102dc4d7b90e57386da2a81a5b13c03498a14943e24086256f60"
+  version "3.5.2"
+  sha256 "fa887340f2998958e98518418e3aa68ba696cae64a6c321932492acb15c3e911"
 
-  url "https://github.com/iamsachin/cowboy/releases/download/v3.5.1/Cowboy_3.5.1_aarch64.dmg"
+  url "https://github.com/iamsachin/cowboy/releases/download/v3.5.2/Cowboy_3.5.2_aarch64.dmg"
   name "Cowboy"
   desc "Track and analyze AI coding agent activity across all your projects"
   homepage "https://github.com/iamsachin/cowboy"
